41d3068e0d8a325ba8859d1c62790844b8416dac
Maria Zherebkina <zherebkina.masha@mail.ru>
2020-10-02 23:38:13 +0300

    Commit change

--- Practice/05/c++/5project/5 проект/5 проект.cpp
+++ Practice/05/c++/5project/5 проект/5 проект.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	double x0, v0, t;
+	double x0, v0, t,xt;
 	double g = 9.8;
 	cout << "Введите значения параметров x0,v0,t" << endl;
 	cin >> x0 >> v0>> t;
-	double x= x0+(v0 * t) - (g*t * t) / 2;
-	cout << x;
+	xt= x0+(v0 * t) - (g*t * t) / 2;
+	cout << "Расстояние, которое объект преодолеет с учётом введённых пользователем параметров"<< abs(xt-x0);
 }
 
