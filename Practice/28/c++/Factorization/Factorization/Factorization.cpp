7e767928a18e3790a2386515c885edc4639b185b
Mary <zherebkina.masha@mail.ru>
2020-12-11 03:28:46 +0300

    25-28

---
+++ Practice/28/c++/Factorization/Factorization/Factorization.cpp
@@ -0,0 +1,40 @@
+﻿#include<vector>
+#include <iostream>
+#include <algorithm>
+#include<map>
+void print_factorization(unsigned int n)
+{
+	std::map <unsigned int, unsigned int> slv;
+	int div = 2;
+	while (n > 1)
+	{
+		for (unsigned int i = 2; i <= n; i++)
+		{
+			if (!(n % i))
+			{
+				n /= i;
+
+				slv[i] = slv.count(i) ? slv[i] + 1 : 1;
+				break;
+			}
+		}
+	}
+	std::cout << "1";
+	for (auto r : slv)
+	{
+		if (r.second > 1)
+		{
+			std::cout << "*" << r.first << "^" << r.second;
+		}
+		else
+		{
+			std::cout << "*" << r.first;
+		}
+	}
+}
+int main()
+{
+	unsigned int n;
+	std::cin >> n;
+	print_factorization(n);
+}
