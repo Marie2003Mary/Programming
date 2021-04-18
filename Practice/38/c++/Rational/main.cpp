88f481f4c9909dbcf9d8e990ac02e612a78d6b7b
Marie2003Mary <71425346+Marie2003Mary@users.noreply.github.com>
2021-04-18 21:36:49 +0300

    Update main.cpp

--- Practice/38/c++/Rational/main.cpp
+++ Practice/38/c++/Rational/main.cpp
@@ -1,20 +1,155 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
-bool equal(double a, double b, double e = 1E-10)
+bool equal(double a, double b, double e = 1E-10);
 
-class Rational {
-    private:
-    int a,b;
-    double x;
+class Rational
+{
+private:
+    double chisl, znam;
+public:
+    Rational()
+    {
+        this->chisl = 0;
+        this->znam = 0;
+    }
+    Rational(int a, int b)
+    {
+        this->chisl = a;
+        this->znam = b;
+    }
+    static int gcd(int p, int v) {
+        int t;
+        if (p < v) {
+            t = p;
+            p = v;
+            v = t;
+        }
+        while (v != 0) {
+            t = v;
+            v = p % v;
+            p = t;
+        }
+        return abs(p);
+    }
+    bool operator== (Rational x)
+    {
+        if (this->znam == 0 && x.chisl == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            if ((this->chisl / this->znam) == (x.chisl / x.znam))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    char is_inf()
+    {
+        if (this->znam != 0)
+        {
+            return 0;
+        }
+        if (this->chisl == 0)
+        {
+            return 0;
+        }
+        return this->chisl > 0 ? 1 : -1;
+    }
+    Rational operator+(Rational x)
+    {
+        if ((this->znam == 0 && this->chisl == 0) || (x.znam == 0 && x.chisl == 0))
+        {
+            return Rational(0, 0);
+        }
+        if (this->is_inf() || x.is_inf())
+        {
+            if (this->is_inf() && x.is_inf())
+            {
+                auto sign = this->is_inf() + x.is_inf();
+                return (sign == 0 && !(this->chisl + x.chisl)) ? Rational(0, 0) : Rational(sign * 1, 0);
+            }
+            return this->is_inf() ? *this : x;
+        }
+        return Rational(this->chisl * x.znam + this->znam * x.chisl, this->znam * x.znam);
+    }
+    Rational operator-(Rational x)
+    {
+        if ((this->znam == 0 && this->chisl == 0) || (x.znam == 0 && x.chisl == 0))
+        {
+            return Rational(0, 0);
+        }
+        if (this->is_inf() || x.is_inf())
+        {
+            if (this->is_inf() && x.is_inf())
+            {
+                auto sign = this->is_inf() + -x.is_inf();
+                return (sign == 0 && !(this->chisl + x.chisl)) ? Rational(0, 0) : Rational(sign * 1, 0);
+            }
+            return this->is_inf() ? *this : Rational(-x.chisl, 0);
+        }
+        return Rational(this->chisl * x.znam - this->znam * x.chisl, this->znam * x.znam);
+    }
+    Rational operator*(Rational x)
+    {
+        return Rational(this->chisl * x.chisl, this->znam * x.znam);
+    }
+    Rational operator/(Rational x)
+    {
+        if ((this->znam == 0 && this->chisl == 0) || (x.znam == 0 && x.chisl == 0))
+        {
+            return Rational(0, 0);
+        }
+        if (this->is_inf() || x.is_inf())
+        {
+            if (this->is_inf() && x.is_inf())
+            {
+                return Rational(0, 0);
+            }
+            return this->is_inf() ?
+                ((double(x) >= 0 && (double(*this) >= 0)
+                    || (double(x) < 0 && (double(*this) < 0))))
+                ? Rational(1, 0) : Rational(-1, 0) : Rational(0, 1);
+        }
+        return Rational(this->chisl * x.znam, this->znam * x.chisl);
+    }
+    operator double()
+    {
+        return this->chisl / this->znam;
+    }
+    operator bool()
+    {
+        if (this->znam != 0 && this->chisl == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    int numerator()
+    {
+        return this->chisl;
+    }
+    int denominator()
+    {
+        return this->znam;
+    }
+    bool isNaN()
+    {
+        return (this->chisl == 0 && this->znam == 0) ? 1 : 0;
+    }
+};
 
-    public:
 
-
-    Rational(int a1 = 0  ,int  b1 = 0 ):a(a1),b(b1){x = a/b};
-        ~Rational() {};
-
-}
 
 int main()
 {
@@ -59,10 +194,10 @@ int main()
         !(Rational(22, 9) == Rational(0, 0)) &&
 
         Rational(0, 1) == Rational(0, 1) &&
-        Rational(0, 1) == Rational(0, 9)  &&
-        Rational(0, 1) == Rational(0, -9)  &&
-        !(Rational(0, 1) == Rational(22, 9))  &&
-        !(Rational(0, 1) == Rational(-22, 9))  &&
+        Rational(0, 1) == Rational(0, 9) &&
+        Rational(0, 1) == Rational(0, -9) &&
+        !(Rational(0, 1) == Rational(22, 9)) &&
+        !(Rational(0, 1) == Rational(-22, 9)) &&
         !(Rational(0, 1) == Rational(22, 0)) &&
         !(Rational(0, 1) == Rational(-22, 0)) &&
         !(Rational(0, 1) == Rational(0, 0)) &&
@@ -126,7 +261,7 @@ int main()
         (Rational(0, 9) + Rational(0, 0)).isNaN() &&
         (Rational(-22, 9) + Rational(0, 0)).isNaN() &&
         (Rational(-22, 0) + Rational(0, 0)).isNaN()
-       ) std::cout << "Summation test passed\n";
+        ) std::cout << "Summation test passed\n";
     else std::cout << "Summation test failed\n";
 
     if ((Rational(22, 0) - Rational(22, 0)).isNaN() &&
@@ -174,9 +309,9 @@ int main()
         Rational(-22, 0) * Rational(22, 0) == Rational(-22, 0) &&
 
         Rational(22, 0) * Rational(22, 9) == Rational(22, 0) &&
-        Rational(22, 9) * Rational(22, 9) == Rational(22*22, 9*9) &&
+        Rational(22, 9) * Rational(22, 9) == Rational(22 * 22, 9 * 9) &&
         Rational(0, 9) * Rational(22, 9) == Rational(0, 9) &&
-        Rational(-22, 9) * Rational(22, 9) == Rational(-22*22, 9*9) &&
+        Rational(-22, 9) * Rational(22, 9) == Rational(-22 * 22, 9 * 9) &&
         Rational(-22, 0) * Rational(22, 9) == Rational(-22, 0) &&
 
         (Rational(22, 0) * Rational(0, 1)).isNaN() &&
@@ -186,9 +321,9 @@ int main()
         (Rational(-22, 0) * Rational(0, 1)).isNaN() &&
 
         Rational(22, 0) * Rational(-22, 9) == Rational(-22, 0) &&
-        Rational(22, 9) * Rational(-22, 9) == Rational(-22*22, 9*9) &&
+        Rational(22, 9) * Rational(-22, 9) == Rational(-22 * 22, 9 * 9) &&
         Rational(0, 9) * Rational(-22, 9) == Rational(0, 9) &&
-        Rational(-22, 9) * Rational(-22, 9) == Rational(22*22, 9*9) &&
+        Rational(-22, 9) * Rational(-22, 9) == Rational(22 * 22, 9 * 9) &&
         Rational(-22, 0) * Rational(-22, 9) == Rational(22, 0) &&
 
         Rational(22, 0) * Rational(-22, 0) == Rational(-22, 0) &&
@@ -243,23 +378,23 @@ int main()
         ) std::cout << "Division test passed\n";
     else std::cout << "Division test failed\n";
 
-    if (equal(Rational(-22, -9), 22/9.0) &&
+    if (equal(Rational(-22, -9), 22 / 9.0) &&
         equal(Rational(-9, -9), 1) &&
-        equal(Rational(-6, -9), 6/9.0) &&
+        equal(Rational(-6, -9), 6 / 9.0) &&
         equal(Rational(0, -9), 0) &&
-        equal(Rational(6, -9), -6/9.0) &&
+        equal(Rational(6, -9), -6 / 9.0) &&
         equal(Rational(9, -9), -1) &&
-        equal(Rational(22, -9), -22/9.0) &&
+        equal(Rational(22, -9), -22 / 9.0) &&
         std::isinf((double)Rational(-9, 0)) &&
         std::isnan((double)Rational(0, 0)) &&
         std::isinf((double)Rational(9, 0)) &&
-        equal(Rational(-22, 9), -22/9.0) &&
+        equal(Rational(-22, 9), -22 / 9.0) &&
         equal(Rational(-9, 9), -1) &&
-        equal(Rational(-6, 9), -6/9.0) &&
+        equal(Rational(-6, 9), -6 / 9.0) &&
         equal(Rational(0, 9), 0) &&
-        equal(Rational(6, 9), 6/9.0) &&
+        equal(Rational(6, 9), 6 / 9.0) &&
         equal(Rational(9, 9), 1) &&
-        equal(Rational(22, 9), 22/9.0)) std::cout << "Conversion to double test passed\n";
+        equal(Rational(22, 9), 22 / 9.0)) std::cout << "Conversion to double test passed\n";
     else std::cout << "Conversion to double test failed\n";
 
     if (Rational(-22, -9) &&
